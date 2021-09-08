0906d84522963bfab98eb2d95e265256bf7c7d5a
Diego Martinez <juandiego337@hotmail.com>
2021-09-07 21:21:26 -0500

    Creating the bars and adding to the board

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@
 
         get elements(){
             let elements = this.bars;
-            elements.push(ball)
+            elements.push(this.ball)
             return elements;
         }
 
@@ -70,9 +70,8 @@
     self.BoardView.prototype = {
 
         draw: function(){
-            for (let i =  board.elements.length -1 ; i >= 0; i--) {
-                let ele = board.elements[i];
-
+            for (let i =  this.board.elements.length -1 ; i >= 0; i--) {
+                let ele = this.board.elements[i];
                 draw(this.ctx, ele);
 
             }
@@ -81,13 +80,14 @@
     }
 
     function draw(ctx, element){
-
-        switch(element.kind){
-            case "square":
-                ctx.fillRect(element.x, element.y, element.width, element.height);
-                break;
+        console.log(element);
+        if(element!=null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
         }
-
     }
 
 })();
@@ -98,9 +98,20 @@ window.addEventListener('load', main);
 //Especie de controlador
 function main(){
 
+
+    //Canvas donde se va a dibujar.
     let canvas = document.getElementById("canvas")
 
+    //Tablero
     let board = new Board(800,400);
+    console.log(board);
+
+    //Barra
+    const bar = new Bar(10,100,40,100,board);
+    const bar2 = new Bar(750, 100, 40, 100, board);
+
+    //Vista del tablero
     let boardView = new BoardView(canvas,board);
+    boardView.draw()
 
 }
