77d95479df814383070c487da8f9f1f94f364dd2
Diego Martinez <juandiego337@hotmail.com>
2021-09-08 00:11:57 -0500

    Ball class created and adding functionality to pause the game

--- js/main.js
+++ js/main.js
@@ -15,9 +15,38 @@
     self.Board.prototype = {
 
         get elements(){
-            let elements = this.bars;
-            //elements.push(this.ball)
+            let elements = this.bars.map((element)=>element); //Paso una copia y no la referencia
+            elements.push(this.ball)
             return elements;
+        }
+
+    }
+
+})();
+
+
+//Crear bola
+
+(function(){
+
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.board = board;
+        this.speedY = 0;
+        this.speedX = 3;
+        this.direction = 1;
+
+        board.ball = this;
+        this.kind = "circle"
+    }
+
+    self.Ball.prototype = {
+
+        move:function(){
+            this.x += this.speedX * this.direction;
+            this.y += this.speedY;
         }
 
     }
@@ -39,7 +68,6 @@
         //Asigno la barra al tablero.
         this.board.bars.push(this);
 
-        //Que voy a dibujar.
         this.kind = "rectangle";
     }
 
@@ -84,8 +112,12 @@
             }
         },
         play: function(){
-            this.clean();
-            this.draw();
+
+            if(this.board.playing){
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+            }
         }
     }
 
@@ -94,6 +126,12 @@
             switch(element.kind){
                 case "rectangle":
                     ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+                case "circle":
+                    ctx.beginPath();
+                    ctx.arc(element.x,element.y,element.radius,0,7);
+                    ctx.fill();
+                    ctx.closePath();
                     break;
             }
 
@@ -107,7 +145,10 @@ let board = new Board(800,400);//Tablero
 const barLeft = new Bar(10,100,40,100,board);//Barra
 const barRight  = new Bar(750, 100, 40, 100, board);//Barra2
 let boardView = new BoardView(canvas,board);//Vista del tablero
+let ball = new Ball(350, 100, 10, board);
 
+
+boardView.draw();
 window.requestAnimationFrame(controller)
 
 document.addEventListener('keydown',(event)=>{
@@ -115,13 +156,20 @@ document.addEventListener('keydown',(event)=>{
     event.preventDefault();
 
     if((event.key)=="ArrowDown"){
+        event.preventDefault();
         barRight.down();
     }else if((event.key)=="ArrowUp"){
+        event.preventDefault();
         barRight.up();
     }else if((event.key)=="w"){
+        event.preventDefault();
         barLeft.up();
     }else if((event.key)=="s"){
+        event.preventDefault();
         barLeft.down();
+    }else if(event.keyCode==32){
+        event.preventDefault();
+        board.playing = !board.playing;
     }
 })
 
